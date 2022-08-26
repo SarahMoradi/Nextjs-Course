a99ae1e83ce80acedaceefc0e9daef48fb590539
Sarah Moradi <moradisarah2001@gamil.com>
2022-08-26 19:50:28 +0430

    added push and replace methods for clickHandler in project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 const HomePage = () => {
-    return ( <>This is Home page</> );
+  const router = useRouter()
+  const clickHandler = () => {
+    //with caching in the history for using back button
+    router.push('/blogs')
+    //without caching in the history for using back button
+    router.replace('/blogs')
+  }
+  return (
+    <>
+      This is Home page <p>go to post page?</p>{' '}
+      <div>
+        <button onClick={clickHandler}>push to blogs</button>
+      </div>
+      <Link href='/posts/1'>
+        <a>post page: post-1</a>
+      </Link>
+    </>
+  )
 }
- 
-export default HomePage;
+
+export default HomePage
